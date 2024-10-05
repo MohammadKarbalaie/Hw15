92f4f08d514a8326f9f1a75bb5c6024fce83dcf4
Mohammad karbalaiemehdi <mohammadkarbalaiemehdi@outlook.com>
2024-10-06 00:22:10 +0330

    fix-show-products

---
+++ HW15/src/components/ProductList.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Product } from "./types";
+
+interface ProductListProps {
+  products: Product[]; 
+  addTocart: (product: Product) => void;
+  remove: (product: Product) => void;
+  cart: { [key: string]: number };
+}
+
+const ProductList: React.FC<ProductListProps> = ({
+  products,
+  addTocart,
+  remove,
+  cart,
+}) => {
+  return (
+    <div className="product-list w-full grid grid-cols-5 px-4 gap-3">
+      {products.map((product, index) => (
+        <div className="bg-[#1e3932] rounded-md py-2" key={index}>
+          <img
+            src={product.img} 
+            alt={product.name}
+            width={150}
+            className="mx-auto"
+          />
+          <p className="text-white font-semibold text-left px-3 py-2">
+            {product.name}
+          </p>
+          <p className="text-[#d8c9a4] font-semibold text-left px-3 py-2">
+            ${product.price.toFixed(2)} 
+          </p>
+
+          <div className="flex text-white items-center justify-center">
+            <button
+              className="bg-yellow-200 text-black px-4 py-1 rounded-tl-md rounded-bl-md"
+              onClick={() => remove(product)}
+            >
+              -
+            </button>
+            <p className="bg-white text-black px-4 py-1">
+              {cart[product.name] || 0}
+            </p>
+            <button
+              className="bg-yellow-200 text-black px-4 py-1 rounded-tr-md rounded-br-md"
+              onClick={() => addTocart(product)}
+            >
+              +
+            </button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ProductList;
--- HW15/src/components/SturbucksUI.tsx
+++ HW15/src/components/SturbucksUI.tsx
@@ -1,6 +1,30 @@
+import React, { useState } from "react";
+import ProductList from "./ProductList";
+import { data } from '../assets/data';
+import { Product } from "./types";
 
 
-export function UI(){
+export const UI: React.FC = () => {
+    const [cart, setCart] = useState<{ [key: string]: number }>({});
+
+    const addToCart = (product: Product) => {
+        setCart((prevCart) => ({
+          ...prevCart,
+          [product.name]: (prevCart[product.name] || 0) + 1,
+        }));
+      };
+    
+      // Handler to remove a product from the cart
+      const removeFromCart = (product: Product) => {
+        setCart((prevCart) => {
+          const newCart = { ...prevCart };
+          if (newCart[product.name] > 0) {
+            newCart[product.name] -= 1;
+          }
+          return newCart;
+        });
+      };
+    
     return(
         <div className='flex flex-col gap-4 bg-[#ececec] pb-5 rounded-md' >
             <div>
@@ -12,62 +36,15 @@ export function UI(){
                     <p className="font-semibold text-xl">Starbucks Online Coffe Order</p>
                 </div>
             </div>
-            <div className='w-full grid grid-cols-5 px-4 gap-3'>
-                <div className="bg-[#1e3932] rounded-md py-2">
-                    <img src="../src/assets/Cappuccino.png" alt="" width={150} className="mx-auto" />
-                    <p className="text-white font-semibold text-left px-3 py-2">Cappuccino</p>
-                    <p className="text-[#d8c9a4] font-semibold text-left px-3 py-2">$3.5</p>
-                    <span className="flex text-white items-center justify-center">
-                    <button className="bg-yellow-200 text-black px-4 py-1 rounded-tl-md rounded-bl-md">-</button>
-                        <p className="bg-white text-black px-4 py-1 ">0</p>
-                    <button className="bg-yellow-200 text-black px-4 py-1  rounded-tr-md rounded-br-md">+</button>
-                    </span>
-                </div>
-                <div className="bg-[#1e3932] rounded-md py-2">
-                    <img src="../src/assets/Latte.png" alt="" width={150} className="mx-auto" />
-                    <p className="text-white font-semibold text-left px-3 py-2">Latte</p>
-                    <p className="text-[#d8c9a4] font-semibold text-left px-3 py-2">$4</p>
-                    <span className="flex text-white items-center justify-center">
-                    <button className="bg-yellow-200 text-black px-4 py-1 rounded-tl-md rounded-bl-md">-</button>
-                        <p className="bg-white text-black px-4 py-1 ">0</p>
-                    <button className="bg-yellow-200 text-black px-4 py-1  rounded-tr-md rounded-br-md">+</button>
-                    </span>
-                </div>
-                <div className="bg-[#1e3932] rounded-md py-2">
-                    <img src="../src/assets/Espresso.png" alt="" width={150} className="mx-auto" />
-                    <p className="text-white font-semibold text-left px-3 py-2">Espresso</p>
-                    <p className="text-[#d8c9a4] font-semibold text-left px-3 py-2">$2.5</p>
-                    <span className="flex text-white items-center justify-center">
-                    <button className="bg-yellow-200 text-black px-4 py-1 rounded-tl-md rounded-bl-md">-</button>
-                        <p className="bg-white text-black px-4 py-1 ">0</p>
-                    <button className="bg-yellow-200 text-black px-4 py-1  rounded-tr-md rounded-br-md">+</button>
-                    </span>
-                </div>
-                <div className="bg-[#1e3932] rounded-md py-2">
-                    <img src="../src/assets/Mocha.png" alt="" width={150} className="mx-auto" />
-                    <p className="text-white font-semibold text-left px-3 py-2">Mocha</p>
-                    <p className="text-[#d8c9a4] font-semibold text-left px-3 py-2">$4.5</p>
-                    <span className="flex text-white items-center justify-center">
-                    <button className="bg-yellow-200 text-black px-4 py-1 rounded-tl-md rounded-bl-md">-</button>
-                        <p className="bg-white text-black px-4 py-1 ">0</p>
-                    <button className="bg-yellow-200 text-black px-4 py-1  rounded-tr-md rounded-br-md">+</button>
-                    </span>
-                </div>
-                <div className="bg-[#1e3932] rounded-md py-2">
-                    <img src="../src/assets/Americano.png" alt="" width={150} className="mx-auto" />
-                    <p className="text-white font-semibold text-left px-3 py-2">Americano</p>
-                    <p className="text-[#d8c9a4] font-semibold text-left px-3 py-2">$3</p>
-                    <span className="flex text-white items-center justify-center">
-                    <button className="bg-yellow-200 text-black px-4 py-1 rounded-tl-md rounded-bl-md">-</button>
-                        <p className="bg-white text-black px-4 py-1 ">0</p>
-                    <button className="bg-yellow-200 text-black px-4 py-1  rounded-tr-md rounded-br-md">+</button>
-                    </span>
-                </div>
-            </div>
 
+            <ProductList
+                products={data}
+                addTocart={addToCart}
+                remove={removeFromCart}
+                cart={cart}
+            />
 
             <h1 className="font-semibold text-xl">Bill</h1>
-
 
             <div className='grid grid-cols-5 w-full px-4 gap-3'>
             <div className="bg-[#1e3932] rounded-md py-2">
